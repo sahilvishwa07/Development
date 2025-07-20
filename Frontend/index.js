4a96624b63e6baab3279d6f28114e8dddc8062db
sahilvishwa07 <sahilvishwakarma711@gmail.com>
2025-07-20 22:39:33 +0530

    Development Part

---
+++ Frontend/index.js
@@ -0,0 +1,79 @@
+// let str="ILOVECODING"9
+
+// console.log(str.slice(1,str.length));
+// veg =str.replace("LOVE","hate");
+// console.log(veg);
+// console.log(str.repeat(2));
+
+// let n = prompt("Write your Number: ");
+// n = parseInt(n);
+// for(let i=n; i<=n*10; i=i+n)
+// {
+//     console.log(i);
+    
+// }
+// for(let i=3; i<=30;i=i+3){
+//     console.log(i);
+    
+// }
+// for (let i=1;i<=3;i++){
+//     console.log('outer loop',i);
+    
+//     for(let j=1;j<=3;j++){
+//         console.log(j);
+        
+//     }
+// }
+// let fav="avatar";
+// let guess = prompt("guess moive");
+// while(( guess != fav) && (guess != "quit")){
+//     guess = prompt(" wrong guess. please try again")
+// }
+// if(guess==fav){
+//     console.log("congrats");
+    
+// }
+// else{
+//     console.log("you quit");
+    
+// }
+// let i=1;
+// while (1<=5){
+//         if(i==3){
+//             break;
+//         }
+//     console.log(i);
+//     i++;
+    
+// }
+// console.log("sahil");
+// loop with array
+// let fruits = ["mango","apple", "banana","lithi","orange"];
+// for(let i=0;i<fruits.length; i++){
+//     console.log(i,fruits[i]);
+    
+// }
+// for (let i=fruits.length-1;i>0;i--){
+//     console.log(i,fruits[i]);
+    
+// }
+// loop with nested array
+// let heros = [
+//     ["ironman","superman","thor"],
+//     ["captain","women","flash"]
+// ]
+// // console.log(heros);
+// for(let i=0;i<heros.length;i++){
+//     console.log(i,heros[i],heros[i].length);
+//     for(let j=0; j<heros[i].length;j++){
+//         console.log(`j=${j}, ${heros[i][j]}`);
+//     }
+// }
+// let student = [ ["aman",95],["shail",65]];
+// for(let i=0; i<student.length;i++){
+//     console.log(`info of student #${i+1}`);
+//     for(let j=0; j<student[i].length; j++){
+//         console.log(student[i][j]);
+        
+//     }
+// }
